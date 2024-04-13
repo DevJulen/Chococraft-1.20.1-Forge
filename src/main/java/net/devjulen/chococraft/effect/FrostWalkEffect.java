f2e36df10109c990fa1e97af5d08adb343fd3700
Julen <julenindias@gmail.com>
2024-04-13 16:37:37 +0200

    Frost Walk effect finished

--- src/main/java/net/devjulen/chococraft/effect/FrostWalkEffect.java
+++ src/main/java/net/devjulen/chococraft/effect/FrostWalkEffect.java
@@ -1,9 +1,10 @@
 package net.devjulen.chococraft.effect;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraftforge.common.Tags;
+import net.minecraft.world.level.block.Blocks;
 
 public class FrostWalkEffect extends MobEffect {
     protected FrostWalkEffect(MobEffectCategory pCategory, int pColor) {
@@ -12,7 +13,15 @@ public class FrostWalkEffect extends MobEffect {
 
     @Override
     public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
-        // if(pLivingEntity.getBlockStateOn().getBlock().getName()) mirar si es agua y convertirlo en hielo etc
+        if(!pLivingEntity.getFeetBlockState().getBlock().equals(Blocks.AIR)) return;
+
+        // Get the block pos of the block under the air block that is on the player's feet
+        BlockPos blockUnderPos = new BlockPos(pLivingEntity.getOnPos().getX(), pLivingEntity.getOnPos().getY() - 1, pLivingEntity.getOnPos().getZ());
+
+        // if the block under the air block on the player's feet is a water block change it to a water block
+        if(pLivingEntity.level().getBlockState(blockUnderPos).getBlock().equals(Blocks.WATER)) {
+            pLivingEntity.level().setBlock(blockUnderPos, Blocks.ICE.defaultBlockState(), 3);
+        }
 
         super.applyEffectTick(pLivingEntity, pAmplifier);
     }
